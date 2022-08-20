de4c2278713bb4867e1604948a4844633b5aa90b
Vinicius Gabriel <vinicius.gabriel@webjump.com.br>
2022-08-20 10:22:04 -0300

    Initial commit with clear laravel and docker configuration

---
+++ app/Http/Controllers/Api/Customer/Create.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api\Customer;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Creates user account
+ */
+class Create extends Controller
+{
+    public function createUser(): bool
+    {
+        return true;
+    }
+}
